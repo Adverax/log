3d7feb4a3bb0bac4af43840d1e17739b1aa4c2a1
Roman_Myakotin <roman.miakotin@proximaresearch.com>
2025-01-14 14:13:11 +0200

    Added parsers

--- exporters/database/exporter.go
+++ exporters/database/exporter.go
@@ -20,6 +20,13 @@ type Exporter struct {
 }
 
 func (that *Exporter) Export(ctx context.Context, entry *log.Entry) {
+	data := that.makeData(entry)
+	fields := that.extractFields(data)
+	args := that.makeQueryArgs(fields)
+	_, _ = that.db.Exec(that.query, args...)
+}
+
+func (that *Exporter) makeData(entry *log.Entry) log.Fields {
 	data := entry.Data.Expand()
 
 	that.fieldMap.EncodePrefixFieldClashes(data)
@@ -30,6 +37,10 @@ func (that *Exporter) Export(ctx context.Context, entry *log.Entry) {
 	data[that.fieldMap.Resolve(log.FieldKeyMsg)] = entry.Message
 	data[that.fieldMap.Resolve(log.FieldKeyLevel)] = entry.Level.String()
 
+	return data
+}
+
+func (that *Exporter) extractFields(data log.Fields) log.Fields {
 	fields := make(log.Fields, 4)
 	for k, v := range data {
 		if kk, ok := that.fieldMap[log.FieldKey(k)]; ok {
@@ -39,17 +50,20 @@ func (that *Exporter) Export(ctx context.Context, entry *log.Entry) {
 	}
 
 	if that.dataKey != "" {
-		raw, err := json.Marshal(entry.Data)
+		raw, err := json.Marshal(data)
 		if err == nil {
 			fields[that.dataKey] = string(raw)
 		}
 	}
 
+	return fields
+}
+
+func (that *Exporter) makeQueryArgs(fields log.Fields) []interface{} {
 	args := make([]interface{}, 0, 32)
 	for _, field := range that.fieldList {
 		v := fields.Fetch(field)
 		args = append(args, v)
 	}
-
-	_, _ = that.db.Exec(that.query, args...)
+	return args
 }
